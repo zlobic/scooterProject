781f11a2caa1173b3d78d86a1be68010fefa8493
mattviy <mrmhamers@gmail.com>
2018-11-27 22:59:38 +0100

    customized map + added localization functionality

--- routes/map-ride.js
+++ routes/map-ride.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 var apiKey = require
 
-
 const apiConfig = require('../config');
 const key = apiConfig.apiKey; 
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
