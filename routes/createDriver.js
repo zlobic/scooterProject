fc7c503445a6b6c12e5ecba38fc7a3a5d6acc195
David Zlobinskyy <bureauzlobi@gmail.com>
2018-11-27 15:40:06 +0100

    Added backend logIn and SignUp Logic

--- routes/createDriver.js
+++ routes/createDriver.js
@@ -5,27 +5,63 @@ const bcrypt = require('bcrypt');
 
 var Driver  = require('../models/driver')
 
+var emailDriver = require('../templates/emailDriver')
+var mjml = require('mjml');
+
 router.post("/", (req,res) => {
-  bcrypt.hash(req.body.password, 5) 
-  .then((hash) => {
-    Driver.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: hash
-    })
-    .then((result) =>{
-      console.log(result);
-      console.log("Succesfully made a new driver");
-      res.render("login");
+  Driver.find({email: req.body.email})
+  .then((result) => {
+    if (result.length > 0){
+      console.log("This email already exists")
+      res.render("emailExists")
+    } 
+    else if (req.body.password != req.body.confirmPassword) {
+      res.render("passwordNoMatch")
+    }
+    else 
+    bcrypt.hash(req.body.password, 5) 
+    .then((hash) => {
+        Driver.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hash,
+      })
+      .then((result) => {
+        var smtpConfig = {
+            service: 'Gmail',
+            auth: {
+                user: 'scootboboff@gmail.com',
+                pass: 'scootbobironhack'
+            } 
+          };
+          let mailOptions = {
+            from: '<scootboboff@gmail.com>', // sender address
+            to: `${result.email}`, // list of receivers
+            subject: 'Welcome to ScootBob!', // Subject line
+            replyTo: `scootboboff@gmail.com`,
+            html: emailDriver(result.firstName).html
+          };  
+          var transporter = nodemailer.createTransport(smtpConfig);
+            transporter.sendMail(mailOptions, (err, res) => {
+            err ? console.log("There was an error: " + err) : console.log("The message was sent succefully")
+          })
+          console.log("Succesfully made a new driver");
+          res.render("login");
+        })
+      .catch((err=> {
+        console.log("Error: " + err)
+      }))
     })
     .catch((err)=>{
-    console.log("Could not create Driver: " + err);
+      console.log("Hashing Error: " + err)
     })
   })
-  .catch((err) => {
-    console.log("Hashing Error: " + err)
-  })
-});
+  .catch((err=>{
+    console.log("Error: " + err);
+  })) 
+})
 
-module.exports = router
+ module.exports = router
+
+
--- routes/drinkerLog.js
+++ routes/drinkerLog.js
@@ -6,28 +6,34 @@ const bcrypt = require('bcrypt');
 
 var Drinker = require('../models/drinker')
 
-
 router.post("/", (req,res) => {
   Drinker.find({email: req.body.email})
   .then((result) => {
     if (result.length > 0) {
-      var password = req.body.password;
-      bcrypt.compare(password, result[0].password)
-      .then((match) => {
-        if (match){
-          res.cookie('loggedIn', 'true', {signed: true});
-          res.cookie('email', result[0].email,{signed: true})
-          res.render("indexDrinker");
-          // HIER GEOCODING 
-        }
-        else res.render("wrongCredentials")
-      })
-      .catch((err)=>{
-        if (err) console.log("Bcrypt Error or Internal Code Error:" + err) 
-      })
-    } 
-    else res.render("usernameFail")
-  }) 
+      if (!result.isVerified) { 
+        return res.render("emailNotVer")
+      }
+      else {
+        var password = req.body.password;
+        bcrypt.compare(password, result[0].password)
+        .then((match) => {
+          if (match){
+            res.send({ token: generateToken(Drinker), user: user.toJSON() });
+            res.cookie('loggedIn', 'true', {signed: true});
+            res.cookie('email', result[0].email)
+            res.render("indexDrinker", {info: result[0].firstName});
+          }
+          else console.log("Wrong password")
+          res.render("emailNotExist");
+        })
+        .catch((err)=> {
+          if (err) console.log("Bcrypt Error or Internal Code Error:" + err) 
+        })
+      }
+    }
+    else console.log("This e-mail does not exist in the database")
+    res.render("emailNotExist");
+  })
   .catch((err) => {
     if (err) console.log("Error:" + err)
   })
