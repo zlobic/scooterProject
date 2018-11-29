b58870d90ffe3cc7904703ea9dcc0ada450324c8
mattviy <mrmhamers@gmail.com>
2018-11-29 11:50:25 +0100

    added display route functionality

--- public/javascript/interaction.js
+++ public/javascript/interaction.js
@@ -20,12 +20,16 @@ $(document).ready(function(){
   function initMap(){
       navigator.geolocation.getCurrentPosition(initialize,fail);
   }
-
+  var directionsService;
+  var directionsDisplay;
+  //Generating the map.
   function initialize(position) {
+      directionsService = new google.maps.DirectionsService;
+      directionsDisplay = new google.maps.DirectionsRenderer;
       var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
       var currentLat = position.coords.latitude; 
       var currentLong = position.coords.longitude;
-   
+
       var mapOptions = {
         zoom: 16,
         center: myLatLng,
@@ -46,12 +50,13 @@ $(document).ready(function(){
   function fail(){
       alert('navigator.geolocation failed, may not be supported');
   }
-   
+  // reverse geocoding from latlng to adress.
   function geocodeLatLng(geocoder, map, currentLat, currentLong) {
   var latlng = {lat: currentLat, lng: currentLong};
   geocoder.geocode({'location': latlng}, function(results, status) {
     if (status === 'OK') {
       if (results[0]) {
+        //updating the starting input field with the users current location.
         $('#origin-input').val(results[0].formatted_address);
         console.log(results[0].formatted_address);
       } else {
@@ -62,5 +67,30 @@ $(document).ready(function(){
       console.log('Geocoder failed due to: ' + status);
     }
   });
+  directionsDisplay.setMap(map);
 
+  var onChangeHandler = function() {
+    console.log("Input is changed");
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  $('#origin-input').on( "change", onChangeHandler )
+  $('#destination-input').on( "change", onChangeHandler )
 }
+//funciton to calculate and display the route based on the origin and destination the user entered. 
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: $('#origin-input').val(),
+    destination: $('#destination-input').val(),
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      // !!!! change to better visible error message for user.
+      console.log('Directions request failed due to ' + status);
+    }
+  });
+}
+
+
+
